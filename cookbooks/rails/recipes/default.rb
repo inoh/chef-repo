e3b1420a62366432b942d59ad84aad39b95698ae
ino_h <syslink.h.inoue@gmail.com>
2015-02-12 06:11:27 +0900

    custom

--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -7,8 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-gem_package "rails"
-gem_package "therubyracer"
+["rails", "therubyracer"].each do |name|
+  gem_package name do
+    options("--no-ri --no-doc")
+  end
+end
 # TODO action install (up to date)
 gem_package "io-console"
 # TODO action install (up to date)
